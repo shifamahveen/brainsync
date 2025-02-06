1c7df1e805fee585a5a71c6c03f719a00c820980
kittukitu <katrakunnabalu@gmail.com>
2025-02-06 11:45:28 +0530

    updated

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 const db = require('../config/db');
 
 exports.getAllUsers = (req, res) => {
-    console.log('Session:', req.session.user); // Log session data for debugging
+    // console.log('Session:', req.session.user); // Log session data for debugging
 
     // Query the database to fetch all users
     db.query('SELECT id, username, email, location, phone, gender, role FROM users', (err, results) => {
@@ -10,7 +10,7 @@ exports.getAllUsers = (req, res) => {
             return res.status(500).send('Internal server error');
         }
         
-        console.log('Fetched users:', results); // Log the fetched users
+        // console.log('Fetched users:', results); // Log the fetched users
 
         // Render the users page and pass the users data
         res.render('admin/users', { users: results, user: req.session.user });
